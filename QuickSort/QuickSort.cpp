329f468f9d1e07200efa914c723cc902c243403d
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-03-28 10:47:27 +0700

    menampilkan output ke layar

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -86,7 +86,9 @@ void q_short(int low, int high)
 }
 
 void display() {
-
+    cout << "\n----------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "------------" << endl;
 }
 
 int main()

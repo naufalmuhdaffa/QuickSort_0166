347f147c6c7383c5266c1c7772ce81b7378891ab
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-03-28 09:53:05 +0700

    menambahkan statement if

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -42,6 +42,13 @@ void q_short(int low, int high)
 {
     int temp;
     int pivot, i, j;
+    if (low > high) { //step 1
+        return;
+    }
+
+    pivot = arr[low]; //step 2
+    i = low + 1; //step 3
+    j = high; //step 4
 }
 
 int main()

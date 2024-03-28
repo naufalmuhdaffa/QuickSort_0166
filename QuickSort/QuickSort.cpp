299389992ee3ff7af388475cd44b2ee9d44296ea
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-03-28 09:40:59 +0700

    menambahkan prosedur input

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -7,6 +7,14 @@ int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movements
 int n;
 
+void input() {
+    while (true)
+    {
+        cout << "Masukan panjang element array: ";
+        cin >> n;
+    }
+}
+
 int main()
 {
     
